fd975d3af32ae43d82f27b90b67d10d06dd56842
Trung <trungnvse@gmail.com>
2025-04-13 15:12:47 +0700

    change and add another file

--- ESP32_DHT11_BLYNK/esp32_dht11_blynk/esp32_dht11_blynk.ino
+++ ESP32_DHT11_BLYNK/esp32_dht11_blynk/esp32_dht11_blynk.ino
@@ -1,26 +1,30 @@
-#define BLYNK_TEMPLATE_ID "TMPL68vcVhXqO"
-#define BLYNK_TEMPLATE_NAME "Check with DHT11"
-#define BLYNK_AUTH_TOKEN "QYb0gDSkL7N5IyUqi2NzKChateHM-fPE"
-
+/*******************************************************************************
+* Include
+*******************************************************************************/
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
-
 #include <DHT.h>
-
+/*******************************************************************************
+* Define
+*******************************************************************************/
+#define BLYNK_TEMPLATE_ID "TMPL68vcVhXqO"
+#define BLYNK_TEMPLATE_NAME "Check with DHT11"
+#define BLYNK_AUTH_TOKEN "QYb0gDSkL7N5IyUqi2NzKChateHM-fPE"
 #define DHTPIN  15
 #define DHTTYPE DHT11
-
+/*******************************************************************************
+* Global variable
+*******************************************************************************/
 DHT dht(DHTPIN, DHTTYPE);
-
 char ssid[] = "FPT 4G";
 char pass[] = "21012024";
-
 float hum = 0;
 float temp = 0;
-
 BlynkTimer timer;
-
+/*******************************************************************************
+* Code
+*******************************************************************************/
 void get_data_from_sensor()
 {
   hum  = dht.readHumidity();
@@ -36,14 +40,15 @@ void Send_data_to_blynk()
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   dht.begin();
   timer.setInterval(2000L, Send_data_to_blynk);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Blynk.run();
   timer.run();
 }
+/*******************************************************************************
+* EOF
+*******************************************************************************/
